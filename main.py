d7bd50211c54ed2596b7b3fd2ea79134a06bdae6
siem beraki <siemberaki@gmail.com>
2022-05-15 15:43:49 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+class player:
+
+
+
+    def  __init__(self,name,age):  #    (self,name,age):
+
+        self.name=name
+        self.age=age
+
+players=[player("siem ",12),player("sham",25)]
+
+
+print(players[0].age)
