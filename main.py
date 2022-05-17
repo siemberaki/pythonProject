3e8a56953dcf09da4aa5fe06a75b20f625e7f1a5
siem beraki <siemberaki@gmail.com>
2022-05-17 14:48:21 +0300

    "inheritance"

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-class item:
-    def calculate_total_price(self, unit, price):
-        return unit * price
+class pet :
+    def __init__ (self,name,age):
+        self.name = name
+        self.age = age
 
 
-item1 = item()
-item1.unit = 2
-item1.price = 3
-print(item1.calculate_total_price(item1.unit, item1.price))
+class dog(pet):
+    def speak(self):
+        print("woowwww")
 
 
-
-
-
+class cat(pet):
+    def speak(self):
+        print("bark")
