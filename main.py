6701c82d84799223ca9ef872f44ca3159a62385c
siem beraki <siemberaki@gmail.com>
2022-05-18 09:47:42 +0300

    "encapsulation"

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-class pet :
-    def __init__ (self,name,age):
-        self.name = name
-        self.age = age
+# Python program to
+# demonstrate protected members
+
+# Creating a base class
+class Base:
+    def __init__(self):
+        # Protected member
+        self._a = 2
 
 
-class dog(pet):
-    def speak(self):
-        print("woowwww")
+# Creating a derived class
+class Derived(Base):
+    def __init__(self):
+        # Calling constructor of
+        # Base class
+        Base.__init__(self)
+        print("Calling protected member of base class: ",
+              self._a)
+
+        # Modify the protected variable:
+        self._a = 3
+        print("Calling modified protected member outside class: ",
+              self._a)
 
 
-class cat(pet):
-    def speak(self):
-        print("bark")
+obj1 = Derived()
+
+obj2 = Base()
+
+# Calling protected member
+# Can be accessed but should not be done due to convention
+print("Accessing protected member of obj1: ", obj1._a)
+
+# Accessing the protected variable outside
+print("Accessing protected member of obj2: ", obj2._a)
